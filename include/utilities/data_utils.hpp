446bdaab91fd1c2ed5480b7f4f173a1e11e0d1a3
rdclab <s24s1037hw@s.chibakoudai.jp>
2025-10-29 20:53:30 +0900

    🐛fix:型名を変更

--- include/utilities/data_utils.hpp
+++ include/utilities/data_utils.hpp
@@ -2,21 +2,21 @@
 
 #include <cstdint>
 
-/*INT*/
-union Int_bytes {
-    int int_value;
+/*INT32*/
+union Int32_bytes {
+    int32_t int_value;
     uint8_t bytes[4];
 };
-inline int bytes_to_int(uint8_t *value) {
-    Int_bytes tmp;
+inline int32_t bytes_to_int32(uint8_t *value) {
+    Int32_bytes tmp;
     tmp.bytes[0]=value[0];
     tmp.bytes[1]=value[1];
     tmp.bytes[2]=value[2];
     tmp.bytes[3]=value[3];
     return tmp.int_value;
 }
-inline void int_to_bytes(uint8_t *value,int int_value){
-    Int_bytes tmp;
+inline void int32_to_bytes(uint8_t *value,int32_t int_value){
+    Int32_bytes tmp;
     tmp.int_value=int_value;
     value[0]=tmp.bytes[0];
     value[1]=tmp.bytes[1];
@@ -24,37 +24,37 @@ inline void int_to_bytes(uint8_t *value,int int_value){
     value[3]=tmp.bytes[3];
 }
 
-/*USHORT*/
-union UShort_bytes{
-    unsigned short short_value;
+/*UINT16*/
+union UInt16_bytes{
+    uint16_t short_value;
     uint8_t bytes[2];
 };
-inline unsigned short bytes_to_ushort(uint8_t *value) {
-    UShort_bytes tmp;
+inline uint16_t bytes_to_uint16(uint8_t *value) {
+    UInt16_bytes tmp;
     tmp.bytes[0]=value[0];
     tmp.bytes[1]=value[1];
     return tmp.short_value;
 }
-inline void ushort_to_bytes(uint8_t *value,unsigned short short_value) {
-    UShort_bytes tmp;
+inline void uint16_to_bytes(uint8_t *value,uint16_t short_value) {
+    UInt16_bytes tmp;
     tmp.short_value=short_value;
     value[0]=tmp.bytes[0];
     value[1]=tmp.bytes[1];
 }
 
-/*SHORT*/
-union Short_bytes{
-    signed short short_value;
+/*INT16*/
+union Int16_bytes{
+    int16_t short_value;
     uint8_t bytes[2];
 };
-inline short bytes_to_short(uint8_t *value){
-    Short_bytes tmp;
+inline int16_t bytes_to_int16(uint8_t *value){
+    Int16_bytes tmp;
     tmp.bytes[0]=value[0];
     tmp.bytes[1]=value[1];
     return tmp.short_value;
 }
-inline void short_to_bytes(uint8_t *value,short short_value){
-    Short_bytes tmp;
+inline void int16_to_bytes(uint8_t *value,int16_t short_value){
+    Int16_bytes tmp;
     tmp.short_value=short_value;
     value[0]=tmp.bytes[0];
     value[1]=tmp.bytes[1];
